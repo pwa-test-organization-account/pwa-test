0ea7fae9e60da38969bab968de6594f27ee08ecf
T-Ikehata <81730900+T-Ikehata@users.noreply.github.com>
2021-04-02 13:07:13 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -25,3 +25,15 @@ self.addEventListener('fetch', function(event) {
             })
     );
 });
+
+// push 通知の待ち受け
+self.addEventListener('push', function(event) {
+  const title = 'Push されました！';
+  const options = {
+    body: event.data.text(),
+    icon: 'icon-192x192.png',
+    badge: 'icon-512x512.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
